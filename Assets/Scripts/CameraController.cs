64a7b861861b33d1cfc93d396750deea8e1a59d5
harpestes <dikaloandrij@gmail.com>
2023-09-20 21:17:28 +0300

    Added first level and character with bahaviour

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 pos;
+
+    void Start()
+    {
+        if (!player)
+        {
+            player = FindObjectOfType<HeroKnight>().transform;
+        }
+    }
+
+    void Update()
+    {
+        pos.x = player.position.x;
+        pos.y = player.position.y + 3f;
+        pos.z = -10f;
+
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
+    }
+}
